b32e8e4cf7cbc0989f009b2c5639b2df1e23e170
Sam Joseph <tansaku@gmail.com>
2016-07-21 20:12:33 +0100

    copies over basics from code climate badges

--- lib/code_climate_project_metrics.rb
+++ lib/code_climate_project_metrics.rb
@@ -1,3 +1,18 @@
-class CodeClimateProjectMetrics
+require 'httparty'
 
+class CodeClimateProjectMetrics
+  def initialize identifier
+    @identifier = identifier
+  end
+
+  def image
+    @identifier.gsub!(/\/$/, '')
+    "https://codeclimate.com/#{@identifier}/badges/gpa.svg"
+  end
+
+  def scalar
+    response = HTTParty.get(gpa_badge_url)
+    stat_regex = /fill-opacity=".3">.*?fill-opacity=".3">([^<]+)/
+    response.body =~ stat_regex ? $1 : nil
+  end
 end
