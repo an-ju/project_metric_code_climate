34fb6aeea76bef22919afa58a7a952d9ea1f40b0
Sam Joseph <tansaku@gmail.com>
2016-07-27 18:53:04 +0900

    adjust instance methods to new scheme

--- lib/project_metric_code_climate.rb
+++ lib/project_metric_code_climate.rb
@@ -1,8 +1,8 @@
 require 'httparty'
 
 class ProjectMetricCodeClimate
-  def initialize identifier
-    @identifier = "github#{URI::parse(identifier).path}"
+  def initialize credentials = {}, raw_data = nil
+    @identifier = "github#{URI::parse(credentials[:url]).path}"
   end
 
   def image
@@ -10,7 +10,7 @@ class ProjectMetricCodeClimate
     "https://codeclimate.com/#{@identifier}/badges/gpa.svg"
   end
 
-  def scalar
+  def score
     response = HTTParty.get(image)
     stat_regex = /fill-opacity=".3">.*?fill-opacity=".3">([^<]+)/
     (response.body =~ stat_regex ? $1 : nil).to_f
--- spec/code_climate_project_metrics_spec.rb
+++ spec/code_climate_project_metrics_spec.rb
@@ -2,17 +2,21 @@ require 'project_metric_code_climate'
 
 describe ProjectMetricCodeClimate do
   context 'AgileVentures/WebsiteOne repo' do
-    subject(:code_climate_project_metrics) { described_class.new 'http://github.com/AgileVentures/WebsiteOne'}
+    subject(:code_climate_project_metrics) do
+      described_class.new url: 'http://github.com/AgileVentures/WebsiteOne'
+    end
 
     it 'has the corresponding scalar value' do
-      expect(code_climate_project_metrics.scalar).to eq 3.5
+      expect(code_climate_project_metrics.score).to eq 3.5
     end
   end
   context 'AgileVentures/LocalSupport repo' do
-    subject(:code_climate_project_metrics) { described_class.new 'http://github.com/AgileVentures/LocalSupport'}
+    subject(:code_climate_project_metrics) do
+      described_class.new url: 'http://github.com/AgileVentures/LocalSupport'
+    end
 
     it 'has the corresponding scalar value' do
-      expect(code_climate_project_metrics.scalar).to eq 3.2
+      expect(code_climate_project_metrics.score).to eq 3.2
     end
   end
 
