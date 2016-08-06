862a9799d3ce029a2a5458407b5a4a2dc0e27915
Sam Joseph <tansaku@gmail.com>
2016-08-06 10:23:18 +0900

    adjusts to return svg directly

--- lib/project_metric_code_climate.rb
+++ lib/project_metric_code_climate.rb
@@ -11,13 +11,13 @@ class ProjectMetricCodeClimate
   end
 
   def image
-    @identifier.gsub!(/\/$/, '')
-    @image ||= "https://codeclimate.com/#{@identifier}/badges/gpa.svg"
+    @raw_data ||= load_remote_image
+    @image ||= raw_data
   end
 
   def score
     stat_regex = /fill-opacity=".3">.*?fill-opacity=".3">([^<]+)/
-    @raw_data ||= HTTParty.get(image).body
+    @raw_data ||= load_remote_image
     @score ||= (@raw_data =~ stat_regex ? $1 : nil).to_f
   end
 
@@ -27,8 +27,19 @@ class ProjectMetricCodeClimate
   end
 
   def refresh
-    @raw_data = HTTParty.get(image).body
+    @raw_data = load_remote_image
     @score = @image = nil
     true
+  end
+
+  private 
+
+  def load_remote_image
+    HTTParty.get(image_url).body
+  end
+
+  def image_url
+    @identifier.gsub!(/\/$/, '')
+    @image ||= "https://codeclimate.com/#{@identifier}/badges/gpa.svg"
   end
 end
